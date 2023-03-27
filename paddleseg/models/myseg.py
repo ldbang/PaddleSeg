23063ffb8b9fa0e926c2f78eefbe44924cde4d57
ldbang <95518835+ldbang@users.noreply.github.com>
2023-03-27 17:38:19 +0800

    Update myseg.py

--- paddleseg/models/myseg.py
+++ paddleseg/models/myseg.py
@@ -1,4 +1,4 @@
-# copyright (c) 2021 PaddlePaddle Authors. All Rights Reserve.
+# copyright (c) 2022 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,83 +23,112 @@ from paddleseg.utils import utils
 
 
 @manager.MODELS.add_component
-class STDCSeg(nn.Layer):
+class my_PPLiteSeg(nn.Layer):
     """
-    The STDCSeg implementation based on PaddlePaddle.
+    The PP_LiteSeg implementation based on PaddlePaddle.
 
-    The original article refers to Meituan
-    Fan, Mingyuan, et al. "Rethinking BiSeNet For Real-time Semantic Segmentation."
-    (https://arxiv.org/abs/2104.13188)
+    The original article refers to "Juncai Peng, Yi Liu, Shiyu Tang, Yuying Hao, Lutao Chu,
+    Guowei Chen, Zewu Wu, Zeyu Chen, Zhiliang Yu, Yuning Du, Qingqing Dang,Baohua Lai,
+    Qiwen Liu, Xiaoguang Hu, Dianhai Yu, Yanjun Ma. PP-LiteSeg: A Superior Real-Time Semantic
+    Segmentation Model. https://arxiv.org/abs/2204.02681".
 
     Args:
-        num_classes(int,optional): The unique number of target classes.
-        backbone(nn.Layer): Backbone network, STDCNet1446/STDCNet813. STDCNet1446->STDC2,STDCNet813->STDC813.
-        use_boundary_8(bool,non-optional): Whether to use detail loss. it should be True accroding to paper for best metric. Default: True.
-        Actually,if you want to use _boundary_2/_boundary_4/_boundary_16,you should append loss function number of DetailAggregateLoss.It should work properly.
-        use_conv_last(bool,optional): Determine ContextPath 's inplanes variable according to whether to use bockbone's last conv. Default: False.
+        num_classes (int): The number of target classes.
+        backbone(nn.Layer): Backbone network, such as stdc1net and resnet18. The backbone must
+            has feat_channels, of which the length is 5.
+        backbone_indices (List(int), optional): The values indicate the indices of output of backbone.
+            Default: [2, 3, 4].
+        arm_type (str, optional): The type of attention refinement module. Default: ARM_Add_SpAttenAdd3.
+        cm_bin_sizes (List(int), optional): The bin size of context module. Default: [1,2,4].
+        cm_out_ch (int, optional): The output channel of the last context module. Default: 128.
+        arm_out_chs (List(int), optional): The out channels of each arm module. Default: [64, 96, 128].
+        seg_head_inter_chs (List(int), optional): The intermediate channels of segmentation head.
+            Default: [64, 64, 64].
+        resize_mode (str, optional): The resize mode for the upsampling operation in decoder.
+            Default: bilinear.
         pretrained (str, optional): The path or url of pretrained model. Default: None.
+
     """
 
     def __init__(self,
                  num_classes,
                  backbone,
-                 use_boundary_2=False,
-                 use_boundary_4=False,
-                 use_boundary_8=True,
-                 use_boundary_16=False,
-                 use_conv_last=False,
+                 backbone_indices=[2, 3, 4],
+                 arm_type='UAFM_SpAtten',
+                 cm_bin_sizes=[1, 2, 4],
+                 cm_out_ch=128,
+                 arm_out_chs=[64, 96, 128],
+                 seg_head_inter_chs=[64, 64, 64],
+                 resize_mode='bilinear',
                  pretrained=None):
-        super(STDCSeg, self).__init__()
+        super().__init__()
 
-        self.use_boundary_2 = use_boundary_2
-        self.use_boundary_4 = use_boundary_4
-        self.use_boundary_8 = use_boundary_8
-        self.use_boundary_16 = use_boundary_16
-        self.cp = ContextPath(backbone, use_conv_last=use_conv_last)
-        self.ffm = FeatureFusionModule(384, 256)
-        self.conv_out = SegHead(256, 256, num_classes)
-        self.conv_out8 = SegHead(128, 64, num_classes)
-        self.conv_out16 = SegHead(128, 64, num_classes)
-        self.conv_out_sp16 = SegHead(512, 64, 1)
-        self.conv_out_sp8 = SegHead(256, 64, 1)
-        self.conv_out_sp4 = SegHead(64, 64, 1)
-        self.conv_out_sp2 = SegHead(32, 64, 1)
+        # backbone
+        assert hasattr(backbone, 'feat_channels'), \
+            "The backbone should has feat_channels."
+        assert len(backbone.feat_channels) >= len(backbone_indices), \
+            f"The length of input backbone_indices ({len(backbone_indices)}) should not be" \
+            f"greater than the length of feat_channels ({len(backbone.feat_channels)})."
+        assert len(backbone.feat_channels) > max(backbone_indices), \
+            f"The max value ({max(backbone_indices)}) of backbone_indices should be " \
+            f"less than the length of feat_channels ({len(backbone.feat_channels)})."
+        self.backbone = backbone
+
+        assert len(backbone_indices) > 1, "The lenght of backbone_indices " \
+            "should be greater than 1"
+        self.backbone_indices = backbone_indices  # [..., x16_id, x32_id]
+        backbone_out_chs = [backbone.feat_channels[i] for i in backbone_indices]
+
+        # head
+        if len(arm_out_chs) == 1:
+            arm_out_chs = arm_out_chs * len(backbone_indices)
+        assert len(arm_out_chs) == len(backbone_indices), "The length of " \
+            "arm_out_chs and backbone_indices should be equal"
+
+        self.ppseg_head = PPLiteSegHead(backbone_out_chs, arm_out_chs,
+                                        cm_bin_sizes, cm_out_ch, arm_type,
+                                        resize_mode)
+
+        if len(seg_head_inter_chs) == 1:
+            seg_head_inter_chs = seg_head_inter_chs * len(backbone_indices)
+        assert len(seg_head_inter_chs) == len(backbone_indices), "The length of " \
+            "seg_head_inter_chs and backbone_indices should be equal"
+        self.seg_heads = nn.LayerList()  # [..., head_16, head32]
+        for in_ch, mid_ch in zip(arm_out_chs, seg_head_inter_chs):
+            self.seg_heads.append(SegHead(in_ch, mid_ch, num_classes))
+
+        # pretrained
         self.pretrained = pretrained
         self.init_weight()
 
     def forward(self, x):
         x_hw = paddle.shape(x)[2:]
-        feat_res2, feat_res4, feat_res8, _, feat_cp8, feat_cp16 = self.cp(x)
 
-        logit_list = []
+        feats_backbone = self.backbone(x)  # [x2, x4, x8, x16, x32]
+        assert len(feats_backbone) >= len(self.backbone_indices), \
+            f"The nums of backbone feats ({len(feats_backbone)}) should be greater or " \
+            f"equal than the nums of backbone_indices ({len(self.backbone_indices)})"
+
+        feats_selected = [feats_backbone[i] for i in self.backbone_indices]
+
+        feats_head = self.ppseg_head(feats_selected)  # [..., x8, x16, x32]
+
         if self.training:
-            feat_fuse = self.ffm(feat_res8, feat_cp8)
-            feat_out = self.conv_out(feat_fuse)
-            feat_out8 = self.conv_out8(feat_cp8)
-            feat_out16 = self.conv_out16(feat_cp16)
+            logit_list = []
 
-            logit_list = [feat_out, feat_out8, feat_out16]
+            for x, seg_head in zip(feats_head, self.seg_heads):
+                x = seg_head(x)
+                logit_list.append(x)
+
             logit_list = [
                 F.interpolate(
-                    x, x_hw, mode='bilinear', align_corners=True)
+                    x, x_hw, mode='bilinear', align_corners=False)
                 for x in logit_list
             ]
-
-            if self.use_boundary_2:
-                feat_out_sp2 = self.conv_out_sp2(feat_res2)
-                logit_list.append(feat_out_sp2)
-            if self.use_boundary_4:
-                feat_out_sp4 = self.conv_out_sp4(feat_res4)
-                logit_list.append(feat_out_sp4)
-            if self.use_boundary_8:
-                feat_out_sp8 = self.conv_out_sp8(feat_res8)
-                logit_list.append(feat_out_sp8)
         else:
-            feat_fuse = self.ffm(feat_res8, feat_cp8)
-            feat_out = self.conv_out(feat_fuse)
-            feat_out = F.interpolate(
-                feat_out, x_hw, mode='bilinear', align_corners=True)
-            logit_list = [feat_out]
+            x = self.seg_heads[0](feats_head[0])
+            x = F.interpolate(x, x_hw, mode='bilinear', align_corners=False)
+            logit_list = [x]
 
         return logit_list
 
@@ -108,110 +137,184 @@ class STDCSeg(nn.Layer):
             utils.load_entire_model(self, self.pretrained)
 
 
+class PPLiteSegHead(nn.Layer):
+    """
+    The head of PPLiteSeg.
+
+    Args:
+        backbone_out_chs (List(Tensor)): The channels of output tensors in the backbone.
+        arm_out_chs (List(int)): The out channels of each arm module.
+        cm_bin_sizes (List(int)): The bin size of context module.
+        cm_out_ch (int): The output channel of the last context module.
+        arm_type (str): The type of attention refinement module.
+        resize_mode (str): The resize mode for the upsampling operation in decoder.
+    """
+
+    def __init__(self, backbone_out_chs, arm_out_chs, cm_bin_sizes, cm_out_ch,
+                 arm_type, resize_mode):
+        super().__init__()
+
+        self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+                                  cm_bin_sizes)
+
+        assert hasattr(layers,arm_type), \
+            "Not support arm_type ({})".format(arm_type)
+        arm_class = eval("layers." + arm_type)
+
+        self.arm_list = nn.LayerList()  # [..., arm8, arm16, arm32]
+        for i in range(len(backbone_out_chs)):
+            low_chs = backbone_out_chs[i]
+            high_ch = cm_out_ch if i == len(
+                backbone_out_chs) - 1 else arm_out_chs[i + 1]
+            out_ch = arm_out_chs[i]
+            arm = arm_class(
+                low_chs, high_ch, out_ch, ksize=3, resize_mode=resize_mode)
+            self.arm_list.append(arm)
+
+    def forward(self, in_feat_list):
+        """
+        Args:
+            in_feat_list (List(Tensor)): Such as [x2, x4, x8, x16, x32].
+                x2, x4 and x8 are optional.
+        Returns:
+            out_feat_list (List(Tensor)): Such as [x2, x4, x8, x16, x32].
+                x2, x4 and x8 are optional.
+                The length of in_feat_list and out_feat_list are the same.
+        """
+
+        high_feat = self.cm(in_feat_list[-1])
+        out_feat_list = []
+
+        for i in reversed(range(len(in_feat_list))):
+            low_feat = in_feat_list[i]
+            arm = self.arm_list[i]
+            high_feat = arm(low_feat, high_feat)
+            out_feat_list.insert(0, high_feat)
+
+        return out_feat_list
+
+
+    
+# class GhostModule(nn.Layer):
+#     def __init__(self,
+#                  in_channels,
+#                  output_channels,
+#                  kernel_size=1,
+#                  ratio=2,
+#                  dw_size=3,
+#                  stride=1,
+#                  relu=True,
+#                  name=None):
+#         super(GhostModule, self).__init__()
+#         init_channels = int(math.ceil(output_channels / ratio))
+#         new_channels = int(init_channels * (ratio - 1))
+#         self.primary_conv = ConvBNLayer(
+#             in_channels=in_channels,
+#             out_channels=init_channels,
+#             kernel_size=kernel_size,
+#             stride=stride,
+#             groups=1,
+#             act="relu" if relu else None,
+#             name=name + "_primary_conv")
+#         self.cheap_operation = ConvBNLayer(
+#             in_channels=init_channels,
+#             out_channels=new_channels,
+#             kernel_size=dw_size,
+#             stride=1,
+#             groups=init_channels,
+#             act="relu" if relu else None,
+#             name=name + "_cheap_operation")
+
+#     def forward(self, inputs):
+#         x = self.primary_conv(inputs)
+#         y = self.cheap_operation(x)
+#         out = paddle.concat([x, y], axis=1)
+#         return out
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class PPContextModule(nn.Layer):
+    """
+    Simple Context module.
+
+    Args:
+        in_channels (int): The number of input channels to pyramid pooling module.
+        inter_channels (int): The number of inter channels to pyramid pooling module.
+        out_channels (int): The number of output channels after pyramid pooling module.
+        bin_sizes (tuple, optional): The out size of pooled feature maps. Default: (1, 3).
+        align_corners (bool): An argument of F.interpolate. It should be set to False
+            when the output size of feature is even, e.g. 1024x512, otherwise it is True, e.g. 769x769.
+    """
+
+    def __init__(self,
+                 in_channels,
+                 inter_channels,
+                 out_channels,
+                 bin_sizes,
+                 align_corners=False):
+        super().__init__()
+
+        self.stages = nn.LayerList([
+            self._make_stage(in_channels, inter_channels, size)
+            for size in bin_sizes
+        ])
+
+        self.conv_out = layers.ConvBNReLU(
+            in_channels=inter_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1)
+#         self.conv_ghost = GhostModule()
+
+        self.align_corners = align_corners
+
+    def _make_stage(self, in_channels, out_channels, size):
+        prior = nn.AdaptiveAvgPool2D(output_size=size)
+        conv = layers.ConvBNReLU(
+            in_channels=in_channels, out_channels=out_channels, kernel_size=1)
+        return nn.Sequential(prior, conv)
+
+    def forward(self, input):
+        out = None
+        input_shape = paddle.shape(input)[2:]
+
+        for stage in self.stages:
+            x = stage(input)
+            x = F.interpolate(
+                x,
+                input_shape,
+                mode='bilinear',
+                align_corners=self.align_corners)
+            if out is None:
+                out = x
+            else:
+                out += x
+
+        out = self.conv_out(out)
+        return out
+
+
 class SegHead(nn.Layer):
     def __init__(self, in_chan, mid_chan, n_classes):
-        super(SegHead, self).__init__()
+        super().__init__()
         self.conv = layers.ConvBNReLU(
-            in_chan, mid_chan, kernel_size=3, stride=1, padding=1)
+            in_chan,
+            mid_chan,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias_attr=False)
         self.conv_out = nn.Conv2D(
-            mid_chan, n_classes, kernel_size=1, bias_attr=None)
+            mid_chan, n_classes, kernel_size=1, bias_attr=False)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.conv_out(x)
         return x
-
-
-class AttentionRefinementModule(nn.Layer):
-    def __init__(self, in_chan, out_chan):
-        super(AttentionRefinementModule, self).__init__()
-        self.conv = layers.ConvBNReLU(
-            in_chan, out_chan, kernel_size=3, stride=1, padding=1)
-        self.conv_atten = nn.Conv2D(
-            out_chan, out_chan, kernel_size=1, bias_attr=None)
-        self.bn_atten = nn.BatchNorm2D(out_chan)
-        self.sigmoid_atten = nn.Sigmoid()
-
-    def forward(self, x):
-        feat = self.conv(x)
-        atten = F.adaptive_avg_pool2d(feat, 1)
-        atten = self.conv_atten(atten)
-        atten = self.bn_atten(atten)
-        atten = self.sigmoid_atten(atten)
-        out = paddle.multiply(feat, atten)
-        return out
-
-
-class ContextPath(nn.Layer):
-    def __init__(self, backbone, use_conv_last=False):
-        super(ContextPath, self).__init__()
-        self.backbone = backbone
-        self.arm16 = AttentionRefinementModule(512, 128)
-        inplanes = 1024
-        if use_conv_last:
-            inplanes = 1024
-        self.arm32 = AttentionRefinementModule(inplanes, 128)
-        self.conv_head32 = layers.ConvBNReLU(
-            128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_head16 = layers.ConvBNReLU(
-            128, 128, kernel_size=3, stride=1, padding=1)
-        self.conv_avg = layers.ConvBNReLU(
-            inplanes, 128, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        feat2, feat4, feat8, feat16, feat32 = self.backbone(x)
-
-        feat8_hw = paddle.shape(feat8)[2:]
-        feat16_hw = paddle.shape(feat16)[2:]
-        feat32_hw = paddle.shape(feat32)[2:]
-
-        avg = F.adaptive_avg_pool2d(feat32, 1)
-        avg = self.conv_avg(avg)
-        avg_up = F.interpolate(avg, feat32_hw, mode='nearest')
-
-        feat32_arm = self.arm32(feat32)
-        feat32_sum = feat32_arm + avg_up
-        feat32_up = F.interpolate(feat32_sum, feat16_hw, mode='nearest')
-        feat32_up = self.conv_head32(feat32_up)
-
-        feat16_arm = self.arm16(feat16)
-        feat16_sum = feat16_arm + feat32_up
-        feat16_up = F.interpolate(feat16_sum, feat8_hw, mode='nearest')
-        feat16_up = self.conv_head16(feat16_up)
-
-        return feat2, feat4, feat8, feat16, feat16_up, feat32_up  # x8, x16
-
-
-class FeatureFusionModule(nn.Layer):
-    def __init__(self, in_chan, out_chan):
-        super(FeatureFusionModule, self).__init__()
-        self.convblk = layers.ConvBNReLU(
-            in_chan, out_chan, kernel_size=1, stride=1, padding=0)
-        self.conv1 = nn.Conv2D(
-            out_chan,
-            out_chan // 4,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            bias_attr=None)
-        self.conv2 = nn.Conv2D(
-            out_chan // 4,
-            out_chan,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            bias_attr=None)
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, fsp, fcp):
-        fcat = paddle.concat([fsp, fcp], axis=1)
-        feat = self.convblk(fcat)
-        atten = F.adaptive_avg_pool2d(feat, 1)
-        atten = self.conv1(atten)
-        atten = self.relu(atten)
-        atten = self.conv2(atten)
-        atten = self.sigmoid(atten)
-        feat_atten = paddle.multiply(feat, atten)
-        feat_out = feat_atten + feat
-        return feat_out
