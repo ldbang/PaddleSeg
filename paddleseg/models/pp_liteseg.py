dc36587b1249378faa432fa3856dfec9d9b2d8b8
ldbang <95518835+ldbang@users.noreply.github.com>
2023-04-04 00:59:36 +0800

    Update pp_liteseg.py

--- paddleseg/models/pp_liteseg.py
+++ paddleseg/models/pp_liteseg.py
@@ -21,7 +21,6 @@ from paddleseg.models import layers
 from paddleseg.cvlibs import manager
 from paddleseg.utils import utils
 
-
 @manager.MODELS.add_component
 class PPLiteSeg(nn.Layer):
     """
@@ -153,11 +152,14 @@ class PPLiteSegHead(nn.Layer):
     def __init__(self, backbone_out_chs, arm_out_chs, cm_bin_sizes, cm_out_ch,
                  arm_type, resize_mode):
         super().__init__()
+#PPModule
 # [1, 2, 4,8]
 #         self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
 #                                   cm_bin_sizes)
     
-            self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+#             self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+#                                   [1, 2, 4,8])
+            self.cm = PPModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
                                   [1, 2, 4,8])
 
         assert hasattr(layers,arm_type), \
@@ -321,3 +323,85 @@ class SegHead(nn.Layer):
         x = self.conv(x)
         x = self.conv_out(x)
         return x
+    
+    
+    
+    
+    
+class PPModule(nn.Layer):
+    """
+    Pyramid pooling module originally in PSPNet.
+    Args:
+        in_channels (int): The number of intput channels to pyramid pooling module.
+        out_channels (int): The number of output channels after pyramid pooling module.
+        bin_sizes (tuple, optional): The out size of pooled feature maps. Default: (1, 2, 3, 6).
+        dim_reduction (bool, optional): A bool value represents if reducing dimension after pooling. Default: True.
+        align_corners (bool): An argument of F.interpolate. It should be set to False when the output size of feature
+            is even, e.g. 1024x512, otherwise it is True, e.g. 769x769.
+    """
+#     def __init__(self,
+#                  in_channels,
+#                  inter_channels,
+#                  out_channels,
+#                  bin_sizes,
+#                  align_corners=False):
+#         super().__init__()
+    def __init__(self, in_channels, out_channels, bin_sizes, dim_reduction,
+                 align_corners):
+        super().__init__()
+
+        self.bin_sizes = bin_sizes
+
+        inter_channels = in_channels
+        if dim_reduction:
+            inter_channels = in_channels // len(bin_sizes)
+
+        # we use dimension reduction after pooling mentioned in original implementation.
+        self.stages = nn.LayerList([
+            self._make_stage(in_channels, inter_channels, size)
+            for size in bin_sizes
+        ])
+
+        self.conv_bn_relu2 = layers.ConvBNReLU(
+            in_channels=in_channels + inter_channels * len(bin_sizes),
+            out_channels=out_channels,
+            kernel_size=3,
+            padding=1)
+
+        self.align_corners = align_corners
+
+    def _make_stage(self, in_channels, out_channels, size):
+        """
+        Create one pooling layer.
+        In our implementation, we adopt the same dimension reduction as the original paper that might be
+        slightly different with other implementations.
+        After pooling, the channels are reduced to 1/len(bin_sizes) immediately, while some other implementations
+        keep the channels to be same.
+        Args:
+            in_channels (int): The number of intput channels to pyramid pooling module.
+            size (int): The out size of the pooled layer.
+        Returns:
+            conv (Tensor): A tensor after Pyramid Pooling Module.
+        """
+
+        prior = nn.AdaptiveAvgPool2D(output_size=(size, size))
+        conv = layers.ConvBNReLU(
+            in_channels=in_channels, out_channels=out_channels, kernel_size=1)
+
+        return nn.Sequential(prior, conv)
+
+    def forward(self, input):
+        cat_layers = []
+        for stage in self.stages:
+            x = stage(input)
+            x = F.interpolate(
+                x,
+                paddle.shape(input)[2:],
+                mode='bilinear',
+                align_corners=self.align_corners)
+            cat_layers.append(x)
+        cat_layers = [input] + cat_layers[::-1]
+        cat = paddle.concat(cat_layers, axis=1)
+        out = self.conv_bn_relu2(cat)
+
+        return out 
