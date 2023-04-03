8d66b10f823650ef2cca7e233af322fc15d9033b
ldbang <95518835+ldbang@users.noreply.github.com>
2023-04-04 01:00:16 +0800

    Update pp_liteseg.py

--- paddleseg/models/pp_liteseg.py
+++ paddleseg/models/pp_liteseg.py
@@ -160,7 +160,7 @@ class PPLiteSegHead(nn.Layer):
 #             self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
 #                                   [1, 2, 4,8])
             self.cm = PPModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
-                                  [1, 2, 4,8])
+                                  [1, 2, 3,6])
 
         assert hasattr(layers,arm_type), \
             "Not support arm_type ({})".format(arm_type)
