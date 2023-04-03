f484d20431650fceee1dd5aef184e4e57fa52652
ldbang <95518835+ldbang@users.noreply.github.com>
2023-04-04 01:05:31 +0800

    Update pp_liteseg.py

--- paddleseg/models/pp_liteseg.py
+++ paddleseg/models/pp_liteseg.py
@@ -152,16 +152,17 @@ class PPLiteSegHead(nn.Layer):
     def __init__(self, backbone_out_chs, arm_out_chs, cm_bin_sizes, cm_out_ch,
                  arm_type, resize_mode):
         super().__init__()
-#PPModule
+#PPModule   ASPPModule
 # [1, 2, 4,8]
 #         self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
 #                                   cm_bin_sizes)
     
 #             self.cm = PPContextModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
 #                                   [1, 2, 4,8])
-            self.cm = PPModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+#             self.cm = PPModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
+#                                   [1, 2, 3,6])
+            self.cm = ASPPModule(backbone_out_chs[-1], cm_out_ch, cm_out_ch,
                                   [1, 2, 3,6])
-
         assert hasattr(layers,arm_type), \
             "Not support arm_type ({})".format(arm_type)
         arm_class = eval("layers." + arm_type)
@@ -405,3 +406,98 @@ class PPModule(nn.Layer):
         out = self.conv_bn_relu2(cat)
 
         return out 
+    
+    
+    
+class ASPPModule(nn.Layer):
+    """
+    Atrous Spatial Pyramid Pooling.
+    Args:
+        aspp_ratios (tuple): The dilation rate using in ASSP module.
+        in_channels (int): The number of input channels.
+        out_channels (int): The number of output channels.
+        align_corners (bool): An argument of F.interpolate. It should be set to False when the output size of feature
+            is even, e.g. 1024x512, otherwise it is True, e.g. 769x769.
+        use_sep_conv (bool, optional): If using separable conv in ASPP module. Default: False.
+        image_pooling (bool, optional): If augmented with image-level features. Default: False
+    """
+
+    def __init__(self,
+                 aspp_ratios,
+                 in_channels,
+                 out_channels,
+                 align_corners,
+                 use_sep_conv=False,
+                 image_pooling=False,
+                 data_format='NCHW'):
+        super().__init__()
+
+        self.align_corners = align_corners
+        self.data_format = data_format
+        self.aspp_blocks = nn.LayerList()
+
+        for ratio in aspp_ratios:
+            if use_sep_conv and ratio > 1:
+                conv_func = layers.SeparableConvBNReLU
+            else:
+                conv_func = layers.ConvBNReLU
+
+            block = conv_func(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=1 if ratio == 1 else 3,
+                dilation=ratio,
+                padding=0 if ratio == 1 else ratio,
+                data_format=data_format)
+            self.aspp_blocks.append(block)
+
+        out_size = len(self.aspp_blocks)
+
+        if image_pooling:
+            self.global_avg_pool = nn.Sequential(
+                nn.AdaptiveAvgPool2D(
+                    output_size=(1, 1), data_format=data_format),
+                layers.ConvBNReLU(
+                    in_channels,
+                    out_channels,
+                    kernel_size=1,
+                    bias_attr=False,
+                    data_format=data_format))
+            out_size += 1
+        self.image_pooling = image_pooling
+
+        self.conv_bn_relu = layers.ConvBNReLU(
+            in_channels=out_channels * out_size,
+            out_channels=out_channels,
+            kernel_size=1,
+            data_format=data_format)
+
+        self.dropout = nn.Dropout(p=0.1)  # drop rate
+
+    def forward(self, x):
+        outputs = []
+        if self.data_format == 'NCHW':
+            interpolate_shape = paddle.shape(x)[2:]
+            axis = 1
+        else:
+            interpolate_shape = paddle.shape(x)[1:3]
+            axis = -1
+        for block in self.aspp_blocks:
+            y = block(x)
+            outputs.append(y)
+
+        if self.image_pooling:
+            img_avg = self.global_avg_pool(x)
+            img_avg = F.interpolate(
+                img_avg,
+                interpolate_shape,
+                mode='bilinear',
+                align_corners=self.align_corners,
+                data_format=self.data_format)
+            outputs.append(img_avg)
+
+        x = paddle.concat(outputs, axis=axis)
+        x = self.conv_bn_relu(x)
+        x = self.dropout(x)
+
+        return x
